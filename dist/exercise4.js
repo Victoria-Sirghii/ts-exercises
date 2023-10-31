83c6a3f1352e80e9aa618153cc9ba2e34b80d34d
Victoria <victoria.sirghii@mail.ebs-integrator.com>
2023-10-31 17:41:08 +0200

    removed node_modules folder

--- dist/exercise4.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-//1. Implement a functions which will return a random element from different types array
-function getRandomElement() { }
-getRandomElement([1, 2, 3, 5]);
-getRandomElement([true, false, true]);
-getRandomElement([{ name: "Andrew" }, { name: "Nicole" }, { name: "Martin" }]);
--- dist/exercise6.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-//Create a type for user using Admin interface
-const user = {
-    username: "simple.user",
-    password: "aaaaaaa",
-};
